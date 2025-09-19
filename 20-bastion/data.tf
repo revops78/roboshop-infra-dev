a0afdb4b33468b8eefd751a894c2e990c68d3354
revops78 <revops78@gmail.com>
2025-09-19 12:26:37 +0100

    for docker

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -18,12 +18,12 @@ output "ami_id" {
   value = data.aws_ami.joindevops.id
 }
 
-data "aws_ssm_parameter" "bastion_sg_id" {
-  name = "/${var.project}/${var.environment}/bastion_sg_id"
-}
+# data "aws_ssm_parameter" "bastion_sg_id" {
+#   name = "/${var.project}/${var.environment}/bastion_sg_id"
+# }
 
-data "aws_ssm_parameter" "public_subnet_ids" {
-  name = "/${var.project}/${var.environment}/public_subnet_ids"
+# data "aws_ssm_parameter" "public_subnet_ids" {
+#   name = "/${var.project}/${var.environment}/public_subnet_ids"
 
-}
+# }
 
